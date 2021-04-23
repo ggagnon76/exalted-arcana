33474fcdcb87f73be99959e875f0ab3cef173bab
ggagnon76 <ggagnon76@hotmail.com>
2021-04-22 22:01:22 -0400

    Actually added the spell casting hook to the main script.

--- scripts/exalted.js
+++ scripts/exalted.js
@@ -1,6 +1,9 @@
 import { hasDependencies } from "./module-dependencies.js";
+import { monitorSpellCasting } from "./spell-active-effects.js"
 
 Hooks.on('ready', () => {
   if (!hasDependencies()) return;
   ui.notifications.info("All module dependencies are installed!");
 })
+
+monitorSpellCasting();
