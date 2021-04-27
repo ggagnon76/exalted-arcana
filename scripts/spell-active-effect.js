3cb325ebc73567cfec4e1c3aa0e7a918d1305440
ggagnon76 <ggagnon76@hotmail.com>
2021-04-26 22:19:43 -0400

    Added code in hook for spell casting

--- scripts/spell-active-effect.js
+++ scripts/spell-active-effect.js
@@ -2,7 +2,16 @@ export function monitorSpellCasting() {
   return Hooks.on('preRollItemBetterRolls', (CustomItemRoll) => {
     const item = CustomItemRoll._item.data.name;
     const myActor = CustomItemRoll._actor.data.name;
-    if (header === "Hold Person") {return ui.notifications.info(myActor + " is casting " + item)};
-    return ui.notifications.info("Not a spell we're watching for.");
+    if (item !== "Hold Person") return ui.notifications.info("Not a spell we're watching for.");
+    const selectedTargets = game.user.targets;
+    const ownedSelected = canvas.tokens.controlled;
+    const targets = [...selectedTargets, ...ownedSelected];
+    if (targets.length === 0) return ui.notifications.info(myActor + " has not selected any targets for the " + item + " spell!");
+    let targetString = "";
+    for (const target of targets) {
+      const name = target.data?.actorData.name || target.data.name;
+      targetString += name + ", ";
+    }
+    ui.notifications.info(myActor + " is casting " + item + "spell with the following targets: " + targetString);
   })
 }
